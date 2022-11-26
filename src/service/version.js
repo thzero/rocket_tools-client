c4195170b22d166daeff042f822c61e2f7e0a09b
thzero <thzero@users.noreply.github.com>
2022-11-25 18:52:33 -0600

    version, opensource, etc. updates

--- src/service/version.js
+++ src/service/version.js
@@ -1,12 +1,12 @@
 import VersionService from '@thzero/library_client/service/version';
 
 // eslint-disable-next-line
-const { version_major, version_minor, version_patch, version_date } = require('../../package.json');
+const { version_major, version_minor, version_patch, version_date, copyright, author, author_url } = require('../../package.json');
 
 class AppVersionService extends VersionService {
 	async _version(correlationId) {
 		// eslint-disable-next-line
-		return this._generate(correlationId, version_major, version_minor, version_patch, version_date);
+		return this._generate(correlationId, version_major, version_minor, version_patch, version_date, copyright, author, author_url);
 	}
 }
 
