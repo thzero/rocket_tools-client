f5ec0a6f87876abb0425d608764181a32decb7b7
thzero <thzero@users.noreply.github.com>
2022-12-31 14:39:02 -0600

    change breakout

--- src/layouts/ContentLayout.vue
+++ src/layouts/ContentLayout.vue
@@ -1,8 +1,8 @@
 <template>
 	<div>
 		<v-row>
-			<v-col cols="0" sm="3" />
-			<v-col cols="12" sm="6">
+			<v-col cols="0" md="3" />
+			<v-col cols="12" md="6">
 	<router-view />
 			</v-col>
 		</v-row>
